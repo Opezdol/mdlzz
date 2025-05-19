1ef1d3339acb78fe97fdaa62851ee0f55e1f364f
pohhmann <pyalich@gmail.com>
2025-05-19 22:44:43 +0300

    din adapter && mesa 7i76 din rail holder

--- mesa_holder/main.py
+++ mesa_holder/main.py
@@ -1,5 +1,11 @@
+from OCP import Geom
 from build123d import (
     Align,
+    GeomType,
+    Select,
+    chamfer,
+    make_hull,
+    SlotOverall,
     loft,
     offset,
     GridLocations,
@@ -30,16 +36,9 @@ from build123d import (
 from dataclasses import dataclass
 from math import sqrt
 
-INCH = 2.54
+from numpy.__config__ import show
 
-
-@dataclass
-class Mesa_Din_Holder:
-    conn_w = 3.7 * INCH
-    conn_r1 = (1.9 - 0.15) * INCH
-    conn_r2 = (3.650 - 0.15) * INCH
-    conn_r3 = (5.45 - 0.15) * INCH
-    conn_r4 = (6.35 - 0.15) * INCH
+INCH = 25.4
 
 
 @dataclass
@@ -47,7 +46,7 @@ class Din_Fix:
     width = 25
     height = 52
     thick = 4
-    clap_inner_h = 43
+    clap_inner_h = 42
     clap_w = 15.5
     clap_offset = 0.5
     clap_h = 3
@@ -64,6 +63,7 @@ class Din_Fix:
             width=self.clap_w + self.clap_offset * 2, height=self.clap_inner_h, radius=2
         )
         res = extrude(sk, amount=self.thick)  # type:ignore
+        res += self.core_fix()
 
         # lip top
         lip_th = (self.height - self.clap_inner_h) / 2
@@ -94,8 +94,37 @@ class Din_Fix:
         res += self.movt()
         res += lip
         res -= lip_enforce
+        res -= self.core_fix_slot()
         # show_object(res, name="body", options={"alpha": 0.5, "color": (64, 164, 223)})
         # show_object(t.movt(), name="movt", options=rand_color(alpha=0.6))  # type:ignore
+        return res
+
+    def core_fix(self, offs=0.0):
+        fix_off = 1.5 + offs
+        fix_w = self.width - 15 + offs
+        sk_minus = Rectangle(self.clap_w, self.clap_inner_h)
+        faces = Sketch() + [
+            RectangleRounded(width=fix_w, height=self.height, radius=2),
+            Pos(0, 0, self.thick / 2)
+            * RectangleRounded(width=fix_w, height=self.height + fix_off * 2, radius=2),
+        ]
+        res = loft(faces, ruled=True)
+        return res - extrude(sk_minus, amount=self.thick / 2)
+
+    def core_fix_minus(self):
+        offs = 0.15
+        sk = RectangleRounded(
+            width=self.width + offs * 2, height=self.height + offs * 2, radius=2
+        )
+        res = extrude(sk, amount=self.thick + offs)  # type:ignore
+        res += self.core_fix(offs=offs)
+        return Pos(0, 0, -self.thick) * res
+
+    def core_fix_slot(self):
+        sk = SlotOverall(width=self.width - 5, height=1.8)
+        res = Pos(
+            0, -self.height / 2 + (self.height - self.clap_inner_h) / 4, 0
+        ) * extrude(sk, amount=self.thick, taper=5)
         return res
 
     def sling3(self):
@@ -141,7 +170,7 @@ class Din_Fix:
 
     def movt(self):
         sk = Circle(radius=2.6 / 2)
-        length = 15
+        length = 14
         rad = 1
         up = 1
         lateral = self.incisio(inc=1.7, length=length - rad * 2)
@@ -191,7 +220,77 @@ class Din_Fix:
         return extrude(sk, amount=length)
 
 
+@dataclass
+class Test_plate:
+    def build(self, fix: Din_Fix):
+        sk = RectangleRounded(width=1.5 * fix.width, height=1.2 * fix.height, radius=2)
+        res = extrude(sk, amount=fix.thick * 1.1)
+        topf = Plane(res.faces().sort_by(Axis.Z)[-1])
+        res -= topf * fix.core_fix_minus()
+        return res
+
+
+@dataclass
+class Mesa_Din_Holder:
+    conn_w = 3.7 * INCH
+    conn_r1 = (1.9 - 0.15) * INCH
+    conn_r2 = (3.650 - 0.15) * INCH
+    conn_r3 = (5.45 - 0.15) * INCH
+    conn_r4 = (6.35 - 0.15) * INCH
+
+    def fixes(self) -> list[Location]:
+        res = [
+            Location((0, -self.conn_w / 2, 0)),
+            Location((0, self.conn_w / 2, 0)),
+            Location((self.conn_r1, -self.conn_w / 2, 0)),
+            Location((self.conn_r1, self.conn_w / 2, 0)),
+            Location((self.conn_r2, -self.conn_w / 2, 0)),
+            Location((self.conn_r2, self.conn_w / 2, 0)),
+            Location((self.conn_r3, -self.conn_w / 2, 0)),
+            Location((self.conn_r3, self.conn_w / 2, 0)),
+            Location((self.conn_r4, -self.conn_w / 2, 0)),
+            Location((self.conn_r4, self.conn_w / 2, 0)),
+        ]
+        return res
+
+    def build(self, locs: list[Location], fix: Din_Fix):
+        sl = Sketch() + [loc * Circle(0.5) for loc in locs]
+        res = offset(make_hull(sl.edges()), amount=5)
+        show_object(extrude(res, amount=5), name="sl")
+
+    def internal_build(self, fix: Din_Fix):
+        sl = Sketch() + [loc * Circle(1) for loc in self.fixes()]
+        sk = Sketch() + [loc * Circle(3.5) for loc in self.fixes()]
+        res = make_hull(sl.edges())
+        res = offset(res, amount=10)
+        # res -= sl
+        body = extrude(res, amount=5)
+        body = chamfer(body.edges().group_by(Axis.Z)[0], length=1)
+        body = chamfer(body.edges().group_by(Axis.Z)[-1], length=1)
+        pins = extrude(sk, amount=7)
+        comb = pins + body
+        res = fillet(
+            new_edges(body, combined=comb)
+            .group_by(Axis.Z)[0]
+            .filter_by(GeomType.CIRCLE),
+            radius=1,
+        )
+
+        botf = Plane(res.faces().sort_by(Axis.Z)[0])
+        res -= botf * Pos(-self.conn_r4 / 3, 0, 0) * fix.core_fix_minus()
+        res -= botf * Pos(self.conn_r4 / 3, 0, 0) * fix.core_fix_minus()
+
+        topf = Plane(res.faces().sort_by(Axis.Z)[-1])
+        sk = Sketch() + [loc * Circle(4.8 / 2) for loc in self.fixes()]
+        res -= extrude(Pos(0, 0, 7) * sk, amount=-3.2)
+        return res
+
+
 t = Din_Fix()
+test = Test_plate()
+msa = Mesa_Din_Holder()
+show_object(msa.internal_build(fix=t), name="hull")
+show_object(test.build(fix=t))
 show_object(
     t.build(), name="Din_Fix", options=rand_color(alpha=0.3)
 )  # type:ignore   )  # type:ignore
